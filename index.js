4034011d358c7f0195e520bcc76740d4b8a886d3
Josué <josuefonseca17@outlook.com>
2024-04-10 10:27:30 -0300

    AdicionadoBotao

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var rndResult1;
 var rndResult2;
 var temp1;
 var temp2
+const btn = document.getElementById("btn");
+
+document.getElementById("dot2Dice1").classList.add("invisible");
+document.getElementById("dot8Dice1").classList.add("invisible");
+document.getElementById("dot5Dice1").classList.add("invisible");
+
+document.getElementById("dot2Dice2").classList.add("invisible");
+document.getElementById("dot8Dice2").classList.add("invisible");
+document.getElementById("dot5Dice2").classList.add("invisible");
+
+btn.addEventListener("click", rollTheDices);
 
 temp1 = Math.random();
 temp1 = temp1 * 6;
@@ -13,50 +24,51 @@ temp2 = temp2 * 6;
 temp2 = Math.floor(temp2);
 temp2 = temp2 + 1;
 
-
-
 rndResult1 = temp1;
 rndResult2 = temp2;
 
+function rollTheDices(){
+
 if(rndResult1 === 1){
-document.getElementById("dot1Dice1").classList.add("invisible");
-document.getElementById("dot2Dice1").classList.add("invisible");
-document.getElementById("dot3Dice1").classList.add("invisible");
-document.getElementById("dot4Dice1").classList.add("invisible");
-document.getElementById("dot6Dice1").classList.add("invisible");
-document.getElementById("dot7Dice1").classList.add("invisible");
-document.getElementById("dot8Dice1").classList.add("invisible");
-document.getElementById("dot9Dice1").classList.add("invisible");
-} else if(rndResult1 === 2){
-document.getElementById("dot1Dice1").classList.add("invisible");
-document.getElementById("dot2Dice1").classList.add("invisible");
-document.getElementById("dot4Dice1").classList.add("invisible");
-document.getElementById("dot6Dice1").classList.add("invisible");
-document.getElementById("dot8Dice1").classList.add("invisible");
-document.getElementById("dot9Dice1").classList.add("invisible");
-document.getElementById("dot5Dice1").classList.add("invisible");
-} else if(rndResult1 === 3){
-document.getElementById("dot1Dice1").classList.add("invisible");
-document.getElementById("dot2Dice1").classList.add("invisible");
-document.getElementById("dot4Dice1").classList.add("invisible");
-document.getElementById("dot6Dice1").classList.add("invisible");
-document.getElementById("dot8Dice1").classList.add("invisible");
-document.getElementById("dot9Dice1").classList.add("invisible");
-} else if(rndResult1 === 4){
-document.getElementById("dot2Dice1").classList.add("invisible");
-document.getElementById("dot4Dice1").classList.add("invisible");
-document.getElementById("dot6Dice1").classList.add("invisible");
-document.getElementById("dot8Dice1").classList.add("invisible");
-document.getElementById("dot5Dice1").classList.add("invisible");
-} else if(rndResult1 === 5){
-document.getElementById("dot2Dice1").classList.add("invisible");
-document.getElementById("dot4Dice1").classList.add("invisible");
-document.getElementById("dot6Dice1").classList.add("invisible");
-document.getElementById("dot8Dice1").classList.add("invisible");
-}else if (rndResult1 === 6){
-document.getElementById("dot2Dice1").classList.add("invisible");
-document.getElementById("dot8Dice1").classList.add("invisible");
-document.getElementById("dot5Dice1").classList.add("invisible");
+    document.getElementById("dot1Dice1").classList.add("invisible");
+    document.getElementById("dot2Dice1").classList.add("invisible");
+    document.getElementById("dot3Dice1").classList.add("invisible");
+    document.getElementById("dot4Dice1").classList.add("invisible");
+    document.getElementById("dot5Dice1").classList.add("vinvisible");
+    document.getElementById("dot6Dice1").classList.add("invisible");
+    document.getElementById("dot7Dice1").classList.add("invisible");
+    document.getElementById("dot8Dice1").classList.add("invisible");
+    document.getElementById("dot9Dice1").classList.add("invisible");
+    } else if(rndResult1 === 2){
+    document.getElementById("dot1Dice1").classList.add("invisible");
+    document.getElementById("dot2Dice1").classList.add("invisible");
+    document.getElementById("dot4Dice1").classList.add("invisible");
+    document.getElementById("dot6Dice1").classList.add("invisible");
+    document.getElementById("dot8Dice1").classList.add("invisible");
+    document.getElementById("dot9Dice1").classList.add("invisible");
+    document.getElementById("dot5Dice1").classList.add("invisible");
+    } else if(rndResult1 === 3){
+    document.getElementById("dot1Dice1").classList.add("invisible");
+    document.getElementById("dot2Dice1").classList.add("invisible");
+    document.getElementById("dot4Dice1").classList.add("invisible");
+    document.getElementById("dot6Dice1").classList.add("invisible");
+    document.getElementById("dot8Dice1").classList.add("invisible");
+    document.getElementById("dot9Dice1").classList.add("invisible");
+    } else if(rndResult1 === 4){
+    document.getElementById("dot2Dice1").classList.add("invisible");
+    document.getElementById("dot4Dice1").classList.add("invisible");
+    document.getElementById("dot6Dice1").classList.add("invisible");
+    document.getElementById("dot8Dice1").classList.add("invisible");
+    document.getElementById("dot5Dice1").classList.add("invisible");
+    } else if(rndResult1 === 5){
+    document.getElementById("dot2Dice1").classList.add("invisible");
+    document.getElementById("dot4Dice1").classList.add("invisible");
+    document.getElementById("dot6Dice1").classList.add("invisible");
+    document.getElementById("dot8Dice1").classList.add("invisible");
+    }else if (rndResult1 === 6){
+    document.getElementById("dot2Dice1").classList.add("invisible");
+    document.getElementById("dot8Dice1").classList.add("invisible");
+    document.getElementById("dot5Dice1").classList.add("invisible");
 }
 
 if(rndResult2 === 1){
@@ -64,6 +76,7 @@ if(rndResult2 === 1){
     document.getElementById("dot2Dice2").classList.add("invisible");
     document.getElementById("dot3Dice2").classList.add("invisible");
     document.getElementById("dot4Dice2").classList.add("invisible");
+    document.getElementById("dot5Dice2").classList.add("vinvisible");
     document.getElementById("dot6Dice2").classList.add("invisible");
     document.getElementById("dot7Dice2").classList.add("invisible");
     document.getElementById("dot8Dice2").classList.add("invisible");
@@ -107,3 +120,5 @@ if(rndResult1 === rndResult2){
 }else{
     document.getElementById("ttl").innerHTML = "Player 2 won!";
  }
+
+}
